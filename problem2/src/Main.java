8558a75037027ac9014125f15d04cccb6d1218fb
EnterYT <97966050+EnterYT@users.noreply.github.com>
2023-04-08 11:27:25 +0600

    Update Main.java

--- problem2/src/Main.java
+++ problem2/src/Main.java
@@ -2,25 +2,25 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static float theAVRG(int[] a, int b){
-        if (b < 1){
+    public static float theAVRG(int[] a, int b){//Function finding the avarage of elements in array a
+        if (b < 1){//Base condition
             return 0;
         }
-        return (theAVRG(a, b - 1) * (b-1) + a[b-1]) / b;
+        return (theAVRG(a, b - 1) * (b-1) + a[b-1]) / b;//Functions calls itself
     }
 
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {//Main function
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int[] b = new int[a];
+        int a = sc.nextInt();//Number of elements to find average
+        int[] b = new int[a];//Array of elements with size of a
 
 
         Scanner tc = new Scanner(System.in);
-        for (int i = 0; i < a; i++){
+        for (int i = 0; i < a; i++){//Filling the array with elements
             b[i] = tc.nextInt();
         }
-        System.out.println(theAVRG(b, a));
+        System.out.println(theAVRG(b, a));//Calls the function to find the average
     }
 }
