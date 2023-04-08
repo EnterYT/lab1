c34b084423ada89af6ad0c291a77dfe7fc997db2
EnterYT <97966050+EnterYT@users.noreply.github.com>
2023-04-08 12:30:51 +0600

    Add files via upload

--- problem4/src/Main.java
+++ problem4/src/Main.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int factorial(int n){
-        if (n == 1)
+    public static int factorial(int n){//Function to find the factorial
+        if (n == 1)//Base condition
             return 1;
-        return n * factorial(n-1);
+        return n * factorial(n-1);//Function calls itself
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {//Main function
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        System.out.println(factorial(a));
+        int a = sc.nextInt();//Number which factorial is needed to find
+        System.out.println(factorial(a));//Calling the function
     }
 }
