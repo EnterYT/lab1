6ddc5ffcd75dc5458a9d9dbd455aeaa8ae2eb21e
EnterYT <97966050+EnterYT@users.noreply.github.com>
2023-04-08 12:34:18 +0600

    Add files via upload

--- problem5/src/Main.java
+++ problem5/src/Main.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
 public class Main {
-    static int fib(int n)
+    static int fib(int n)//Function for finding nth number in fibonacci sequence
     {
-        if (n <= 1)
+        if (n <= 1)//Base condition
             return n;
-        return fib(n - 1) + fib(n - 2);
+        return fib(n - 1) + fib(n - 2);//The function calls itself
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {//Main function
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        System.out.println(fib(a));
+        int a = sc.nextInt();//Entering the number
+        System.out.println(fib(a));//Calls the function
     }
 }
