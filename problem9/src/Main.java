23310533418595e922ad757d5ae6002203b6365d
EnterYT <97966050+EnterYT@users.noreply.github.com>
2023-04-08 12:46:43 +0600

    Add files via upload

--- problem9/src/Main.java
+++ problem9/src/Main.java
@@ -2,17 +2,17 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static int biCoef(int n, int k){
-        if (n == k || n == 0 || k ==0){
+    public static int biCoef(int n, int k){//Function finding binomial Coefficient
+        if (n == k || n == 0 || k ==0){//Base condition
             return 1;
         }
-        return (biCoef(n - 1, k) + biCoef(n-1, k-1));
+        return (biCoef(n - 1, k) + biCoef(n-1, k-1));//Function calls itself
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {//Main function
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
+        int a = sc.nextInt();//Entering the values
         int b = sc.nextInt();
-        System.out.println(biCoef(a, b));
+        System.out.println(biCoef(a, b));//Calls the function
     }
 }
