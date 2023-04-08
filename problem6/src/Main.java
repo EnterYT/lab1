8e3ddf71c79144dca50482787a7ee4787c1b26cc
EnterYT <97966050+EnterYT@users.noreply.github.com>
2023-04-08 12:37:45 +0600

    Add files via upload

--- problem6/src/Main.java
+++ problem6/src/Main.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int thePOW(int a, int b){
-        if (b == 0)
+    public static int thePOW(int a, int b){//Function for finding the bth power of number a
+        if (b == 0)//Base condition
             return 1;
-        return a * thePOW(a, b - 1);
+        return a * thePOW(a, b - 1);//Function calls itself
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {//Main function
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        int a = sc.nextInt();//The number
+        int b = sc.nextInt();//The power of number
         System.out.println(thePOW(a, b));
     }
 }
